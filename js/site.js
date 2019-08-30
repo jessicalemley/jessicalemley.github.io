c27d5a47fbea0632f39e94db6e56b88bb791bb13
Jessica Lemley <lemley.jess@gmail.com>
2019-08-30 15:35:23 -0500

    wired up contact modal

--- js/site.js
+++ js/site.js
@@ -9,7 +9,7 @@ page = function () {
                 overlay.addClass("show")
             }
         },
-        copyNav: function() {
+        copyNav: function () {
             let nav = $(".navbar-nav").clone();
             nav.removeClass("navbar-nav");
             nav.addClass("__nav-overlay");
@@ -21,17 +21,41 @@ page = function () {
     return {
 
         init: function () {
-            $(".navbar-toggler").click(
+            $(".navbar-toggler").on("click",
                 function () {
                     pvt.showNav();
                 }
             );
             pvt.copyNav();
-            $(".modal-close-nav-overlay").click(
+            $(".modal-close-nav-overlay").on("click",
                 function (ele) {
                     $(".__nav-overlay").removeClass("show")
                 }
-            )
+            );
+
+            // Get the modal
+            let modal = $("#myModal");
+
+// Get the button that opens the modal
+            $("#contact").click(
+                function (e) {
+                    $("#myModal").addClass("show")
+                }
+            );
+
+// Get the <span> element that closes the modal
+            modal.find(".close").on("click",
+                function (e) {
+                    $("#myModal").removeClass("show")
+
+                }
+            );
+
+
+// When the user clicks anywhere outside of the modal, close it
+            $(window).on("click", function (event) {
+                $("#myModal").removeClass("hide")
+            });
         }
 
     };
