e2442a5f8acc14b5b4519b8d8a93280306d62fff
Jessica Lemley <lemley.jess@gmail.com>
2019-09-05 21:22:32 -0500

    fixed nav overlay, fixed modal issues

--- js/site.js
+++ js/site.js
@@ -2,60 +2,59 @@ page = function () {
 
     let pvt = {
         showNav: function () {
-            let overlay = $(".__nav-overlay");
-            if (overlay.hasClass("show")) {
-                overlay.removeClass("show")
-            } else {
-                overlay.addClass("show")
-            }
+            $(".__nav-overlay").addClass("show");
+            $(".navbar-collapse").removeClass("show");
         },
         copyNav: function () {
             let nav = $(".navbar-nav").clone();
             nav.removeClass("navbar-nav");
             nav.addClass("__nav-overlay");
             nav.appendTo($("body"))
-        }
-
-    };
-
-    return {
-
-        init: function () {
+        },
+        setupMobileNavToggle: function () {
             $(".navbar-toggler").on("click",
                 function () {
                     pvt.showNav();
                 }
             );
-            pvt.copyNav();
             $(".modal-close-nav-overlay").on("click",
                 function (ele) {
-                    $(".__nav-overlay").removeClass("show")
+                    $(".__nav-overlay").removeClass("show");
+                    $(".navbar-collapse").removeClass("show");
                 }
             );
-
-            // Get the modal
-            let modal = $("#myModal");
-
-// Get the button that opens the modal
+        },
+        setupShowContactModal: function () {
             $("#contact").click(
                 function (e) {
-                    $("#myModal").addClass("show")
+                    $("#myModal").addClass("show");
+                    $(".parallax").addClass("modal-visible");
+                    e.stopImmediatePropagation();
                 }
             );
 
-// Get the <span> element that closes the modal
-            modal.find(".close").on("click",
-                function (e) {
-                    $("#myModal").removeClass("show")
-
-                }
-            );
-
-
-// When the user clicks anywhere outside of the modal, close it
-            $(window).on("click", function (event) {
-                $("#myModal").removeClass("hide")
+            $(".modal-content").on("click", function (e) {
+                e.stopImmediatePropagation();
             });
+
+            $("#myModal").find(".close").on("click",
+                function (e) {
+                    $("#myModal").removeClass("show");
+                    $(".parallax").removeClass("modal-visible");
+                }
+            );
+            $(window).on("click", function (event) {
+                $("#myModal").removeClass("show");
+                $(".parallax").removeClass("modal-visible");
+            });
+        }
+    };
+
+    return {
+        init: function () {
+            pvt.copyNav();
+            pvt.setupMobileNavToggle();
+            pvt.setupShowContactModal();
         }
 
     };
